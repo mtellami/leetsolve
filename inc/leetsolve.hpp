6ee7ac58dd9028d8ca1395a0865b33dafd35fb6a
mtellami <moadtell@gmail.com>
2023-11-04 01:33:37 +0100

    level 00

--- inc/leetsolve.hpp
+++ inc/leetsolve.hpp
@@ -3,6 +3,27 @@
 #include <iostream>
 #include <vector>
 #include <limits.h>
+#include <sys/resource.h>
+#include <chrono>
 
 using namespace std;
 
+template <typename T, typename... Args>
+T measure(T (*func)(Args...), Args... args) {
+  // struct rusage start, end;
+  // getrusage(RUSAGE_SELF, &start);
+
+  auto start_time = chrono::high_resolution_clock::now();
+
+	// Function call
+  T result = func(args...);
+	cout << result << endl;
+
+  auto end_time = chrono::high_resolution_clock::now();
+  // getrusage(RUSAGE_SELF, &end);
+
+	auto execution_time = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count();
+  cout << "Time taken by the function: " << execution_time << " microseconds" << endl;
+
+  return result;
+}
--- src/lcs.cpp
+++ src/lcs.cpp
@@ -1,28 +1,27 @@
 #include "leetsolve.hpp"
 
-size_t lcs(string s1, string s2, size_t len1, size_t len2) {
-  if (!len1 || !len2)
-	return 0;
-
-  if (s1[len1 - 1] == s2[len2 - 1])
-	return 1 + lcs(s1, s2, len1 - 1, len2 - 1);
-
-  size_t x = lcs(s1, s2, len1, len2 - 1);
-  size_t y = lcs(s1, s2, len1 - 1, len2);
-  return max(x, y);
+// using Recursion
+size_t lcs(string s1, string s2) {
+  if (!s1.length() || !s2.length()) {
+		return 0;
+	}
+  if (s1.back() == s2.back()) {
+		return 1 + lcs(s1.substr(0, s1.length() - 1), s2.substr(0, s2.length() - 1));
+	}
+  return max(lcs(s1, s2.substr(0, s2.length() - 1)), lcs(s1.substr(0, s1.length() - 1), s2));
 }
 
 int main(int argc, char **argv) {
   if (argc != 3) {
-		cerr << "usage: ./lcs <string1> <string2>" << endl;
+		cerr << "try: ./lcs <string1> <string2>" << endl;
 		return EXIT_FAILURE;
   }
 
   string s1(argv[1]);
   string s2(argv[2]);
 
-  size_t result = lcs(s1, s2, s1.length(), s2.length());
-
+  size_t result = lcs(s1, s2);
   cout << "Longest common subsequence: " << result << endl;
+
   return EXIT_SUCCESS;
 }
